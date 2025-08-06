1930ef5cdd1ebf0142c8172e7d4b40756978fcd8
kabaadpe01-coder <kabaadpe01@gmail.com>
2025-08-06 14:46:36 +0530

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Header from '../Header';
+import Hero from '../Hero';
+import Features from '../Features';
+import Leaderboard from '../Leaderboard';
+import Testimonials from '../Testimonials';
+import Footer from '../Footer';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <Leaderboard />
+      <Testimonials />
+      <Footer />
+    </>
+  );
+}
